d6351425b0722374240e2b172f5b6c27efd85598
baloghdominik <balogh0dominik@gmail.com>
2018-04-12 17:29:08 +0200

    Adding registration

--- src/main/java/com/greenfoxacademy/baloghdominik/mysql/controllers/RegisterController.java
+++ src/main/java/com/greenfoxacademy/baloghdominik/mysql/controllers/RegisterController.java
@@ -8,16 +8,14 @@ import com.greenfoxacademy.baloghdominik.mysql.services.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Random;
 
 @Controller
+@RequestMapping("/register")
 public class RegisterController {
 
     private UserModelsRepository userModelsRepository;
@@ -29,13 +27,13 @@ public class RegisterController {
         this.validation = validation;
     }
 
-    @GetMapping(value={"register", "register/"})
+    @GetMapping(value={"", "/"})
     public String register(Model model) {
 
         return "register";
     }
 
-    @GetMapping(value = "register/newuser")
+    @PostMapping(value = "/newuser")
     public String add(@ModelAttribute(value="username") String username, @ModelAttribute(value = "password") String password,
                       @ModelAttribute(value = "passwordConfirmation") String passwordConfirmation, HttpServletResponse response) {
 
